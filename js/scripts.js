57bb80b1fd416bb7720ad6c1e920037c3fb3b996
Dima <dimonsmail8@gmail.com>
2015-05-11 18:21:18 +0300

    add map

--- js/scripts.js
+++ js/scripts.js
@@ -135,31 +135,146 @@ $(document).ready(function () {
     });
 
 
-
-
-
-
+    // MAP
     var map = L.map('map').setView([59.932040, 30.356126], 16);
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
     }).addTo(map);
 
-    //var littleton = L.marker([59.931765, 30.354620]).bindPopup('ст. метро «Маяковская»');
-
-    //littleton.addTo(map);
-
-
-
-
-    var myIcon = L.icon({
-        iconUrl: '../img/my-icon.png',
+    // big marker
+    var bigIcon = L.icon({
+        iconUrl: '../img/big-icon.png',
         iconSize: [43, 63]
     });
 
-    L.marker([59.932157, 30.351523], {
-        icon: myIcon
+    L.marker([59.932050, 30.350842], {
+        icon: bigIcon
     }).addTo(map);
 
+    // icons
+    var metroIcon = L.divIcon({
+            className: 'transport-icon on-foot',
+            iconSize: [39, 39]
+        }),
+        MoscowStationIcon = L.divIcon({
+            className: 'transport-icon train',
+            iconSize: [39, 39]
+        }),
+        FinlandStationIcon = L.divIcon({
+            className: 'transport-icon train',
+            iconSize: [39, 39]
+        }),
+        PulkovoAirportIcon = L.divIcon({
+            className: 'transport-icon plane',
+            iconSize: [39, 39]
+        }),
+        SeaPortIcon = L.divIcon({
+            className: 'transport-icon ship',
+            iconSize: [39, 39]
+        });
 
+
+    // markers
+    var metroMarker = L.marker([59.931776, 30.354581], {
+            icon: metroIcon
+        }).bindPopup("ст. метро<br>«Маяковская»").addTo(map),
+        MoscowStationMarker = L.marker([59.930103, 30.361997], {
+            icon: MoscowStationIcon
+        }).bindPopup("Московский<br>ж/д вокзал").addTo(map),
+        FinlandStationMarker = L.marker([59.955539, 30.356345], {
+            icon: FinlandStationIcon
+        }).bindPopup("Финляндский<br>ж/д вокзал").addTo(map),
+        PulkovoAirportMarker = L.marker([59.797388, 30.273487], {
+            icon: PulkovoAirportIcon
+        }).bindPopup("Аэропорт<br>«Пулково»").addTo(map),
+        SeaPortMarker = L.marker([59.948942, 30.194941], {
+            icon: SeaPortIcon
+        }).bindPopup("Пассажирский<br>морской терминал").addTo(map);
+
+    var pointA = new L.LatLng(59.955272, 30.356104),
+        pointB = new L.LatLng(59.955334, 30.355697),
+        pointC = new L.LatLng(59.955306, 30.355104),
+        pointD = new L.LatLng(59.955792, 30.351217),
+        pointE = new L.LatLng(59.953843, 30.349887),
+        pointF = new L.LatLng(59.949523, 30.348925),
+        pointG = new L.LatLng(59.932620, 30.347884),
+        pointH = new L.LatLng(59.932230, 30.351011),
+
+        pointAA = new L.LatLng(59.796799, 30.277353),
+        pointAB = new L.LatLng(59.790065, 30.324413),
+        pointAC = new L.LatLng(59.916753, 30.318088),
+        pointAD = new L.LatLng(59.927925, 30.346832),
+        pointAE = new L.LatLng(59.929460, 30.347692),
+        pointAF = new L.LatLng(59.932614, 30.347898),
+
+        pointBA = new L.LatLng(59.948864, 30.195885),
+        pointBB = new L.LatLng(59.948146, 30.195587),
+        pointBC = new L.LatLng(59.947034, 30.203536),
+        pointBD = new L.LatLng(59.946924, 30.207108),
+        pointBE = new L.LatLng(59.945952, 30.214246),
+        pointBF = new L.LatLng(59.944686, 30.214054),
+        pointBG = new L.LatLng(59.943949, 30.214373),
+        pointBH = new L.LatLng(59.939628, 30.218176),
+        pointBI = new L.LatLng(59.941105, 30.224789),
+        pointBJ = new L.LatLng(59.939320, 30.228782),
+        pointBK = new L.LatLng(59.937489, 30.234193),
+        pointBL = new L.LatLng(59.938595, 30.245070),
+        pointBM = new L.LatLng(59.946218, 30.271620),
+        pointBN = new L.LatLng(59.938479, 30.280616),
+        pointBO = new L.LatLng(59.941437, 30.290717),
+        pointBP = new L.LatLng(59.938130, 30.294529),
+        pointBQ = new L.LatLng(59.941015, 30.304075),
+        pointBR = new L.LatLng(59.942262, 30.306425),
+        pointBS = new L.LatLng(59.938100, 30.312610),
+        pointBT = new L.LatLng(59.937056, 30.312421),
+
+        PointsListA = [pointA, pointB, pointC, pointD, pointE, pointF, pointG, pointH],
+        PointsListB = [pointAA, pointAB, pointAC, pointAD, pointAE, pointG, pointH],
+        PointsListC = [pointBA, pointBB, pointBC, pointBD, pointBE, pointBF, pointBG, pointBH, pointBI, pointBJ, pointBK, pointBL, pointBM, pointBN, pointBO, pointBP, pointBQ, pointBR, pointBS, pointBT, pointH];
+
+    var FinlandStationRoud = new L.Polyline(PointsListA),
+        AirportRoud = new L.Polyline(PointsListB),
+        PortRoud = new L.Polyline(PointsListC);
+
+    var deleteLines = function () {
+        map.removeLayer(FinlandStationRoud);
+        map.removeLayer(AirportRoud);
+        map.removeLayer(PortRoud);
+    }
+
+    // events
+    $('#mayakovskaya').live("click", function () {
+        metroMarker.openPopup().addTo(map);
+        map.setView([59.932040, 30.356126]);
+        deleteLines();
+    });
+
+    $('#moscow-station').live("click", function () {
+        MoscowStationMarker.openPopup().addTo(map);
+        map.setView([59.930103, 30.361997]);
+        deleteLines();
+    });
+
+    $('#finland-station').live("click", function () {
+        FinlandStationMarker.openPopup().addTo(map);
+        map.setView([59.955539, 30.356345]);
+        deleteLines();
+        FinlandStationRoud.addTo(map);
+    });
+
+    $('#pulkovo-airport').live("click", function () {
+        PulkovoAirportMarker.openPopup().addTo(map);
+        map.setView([59.797388, 30.273487]);
+        deleteLines();
+        AirportRoud.addTo(map);
+    });
+
+    $('#sea-port').live("click", function () {
+        SeaPortMarker.openPopup().addTo(map);
+        map.setView([59.948942, 30.194941]);
+        deleteLines();
+        PortRoud.addTo(map);
+    });
 });
